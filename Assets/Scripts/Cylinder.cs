8f873e5086b969c54ac263db653f7ec604408fde
Dennis Hackethal <dh@dennishackethal.com>
2024-01-04 17:51:14 -0700

    Add cylinder
    Since the challenge calls for a third shape.

---
+++ Assets/Scripts/Cylinder.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cylinder : Shape
+{
+    public Cylinder()
+    {
+        Color = "red";
+        Name = "cylinder";
+    }
+}
